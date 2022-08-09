02f11cb208336ef022a55d29f06718d08b5908ef
jyoti <jyotidhage10@gmail.com>
2022-08-09 16:02:49 +0530

    [Aaradhya]Add:Logiacal Program Assignment Completed

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace LogicalPrograms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            FibonacciNumber.FibonacciNumberSeries();
+            PerfectNumbers.PerfectNumbersBetween();
+            ReverseNumbers.ReverseNumberSolution();
+            PrimeNumber.PrimeNumberSolution();
+            CoupanNumber.CoupanNumberSolution();
+            Stopwatch1.SimulateStopWatch();
+          
+
+
+
+
+        }
+    }
+}
