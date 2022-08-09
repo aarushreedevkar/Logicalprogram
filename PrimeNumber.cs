32804cf5cec002b9073d164a21f00a1366f08828
jyoti <jyotidhage10@gmail.com>
2022-08-09 16:15:56 +0530

    [Aaradhya]Add:Prime Number solution

---
+++ PrimeNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public static class PrimeNumber
+    {
+        public static void PrimeNumberSolution()
+        {
+            int n,  m = 0 ;
+            Console.Write("Enter the Number to check Prime: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            m = n / 2;
+            
+            
+                if (m == 0)
+                {
+                    Console.Write("Number is  not   Prime.");
+                    
+                   
+                }
+            else
+            {
+                Console.Write("Number is  Prime.");
+
+            }
+        }
+            
+              
+        }
+    }
+    
+
